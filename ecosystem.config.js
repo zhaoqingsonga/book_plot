5819a41846b120a5d13aeb86337ff00f47bd263e
zhaoqingsonga <zhaoqingsonga@163.com>
2026-04-26 12:31:39 +0800

    fix: update PM2 cwd path to book_plot

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: "field-book",
     script: "./run.sh",
-    cwd: "/Users/m4/Documents/GitHub/melt/make_field_book",
+    cwd: "/Users/m4/Documents/GitHub/book_plot",
     interpreter: "/bin/bash",
     env: {
       PORT: 3681
